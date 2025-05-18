c1e31df53657ffc6d996ee9da5f669824b73b6f5
laobamac <wxcznb@qq.com>
2025-05-18 14:32:55 +0800

    Update config.php

--- webui/config.py
+++ webui/config.py
@@ -10,20 +10,21 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # 邮件配置 - 不安全的SSL
-    MAIL_SERVER = 'smtp.yeah.net'  # SMTP服务器地址
-    MAIL_PORT = 465  # SSL端口
-    MAIL_USE_SSL = True
-    MAIL_USE_TLS = False  # 确保TLS关闭
-    MAIL_USERNAME = 'simplehac@yeah.net'
-    MAIL_PASSWORD = 'WLLTHYuvT2Rt3Wqb'
-    MAIL_DEFAULT_SENDER = 'simplehac@yeah.net'
+    # 邮件配置
+    MAIL_SERVER = ''  # SMTP服务器地址
+    MAIL_PORT = 465  # 端口
+    MAIL_USE_SSL = True # SSL
+    MAIL_USE_TLS = False  # TLS
+    MAIL_USERNAME = '' # 邮件用户名
+    MAIL_PASSWORD = '' # 密码
+    MAIL_DEFAULT_SENDER = '' # 发件人，通常等于用户名
     
     # 禁用SSL证书验证
     MAIL_SUPPRESS_SEND = False  # 确保邮件发送不被抑制
     MAIL_DEBUG = True  # 调试模式可以看到更多信息
     
     # 应用配置
-    APP_NAME = 'SimpleKeytime'
-    APP_DESCRIPTION = '简单易用的软件授权管理系统'
-    APP_URL = os.getenv('APP_URL', 'http://localhost:5000')
+    APP_NAME = 'SimpleKeytime' # 网站名
+    APP_DESCRIPTION = '简单易用的软件授权管理系统' # 描述
+    APP_URL = os.getenv('APP_URL', 'http://localhost:5000') # 变量调用的站点地址
+    APP_PORT = 5000
